bb261aaecfdaf829662f46b3beef3b564d927718
Ishaan Garg <ishaangarg.cse19@chitkarauniversity.edu.in>
2022-06-21 16:30:22 +0530

    Initial commit

---
+++ backend/data/products.js
@@ -0,0 +1,148 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Drone',
+    image: '/images/Drone.jpg',
+    description:
+      'This Drone has a 4 Rotor Design with 6 Axis Gyroscope, which Achieves super Wind Performance',
+    brand: 'Ryze Tello',
+    category: 'Electronics',
+    price: 120.11,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple Macbook',
+    image: '/images/Macbook.jpg',
+    description:
+      'Our most powerful notebooks. Fast M1 processors, incredible graphics, and spectacular Retina displays',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 899.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Apple Watch',
+    image: '/images/AppleWatch.jpg',
+    description:
+      'A line of smartwatches produced by Apple Inc. It incorporates fitness tracking, health-oriented capabilities, and wireless telecommunication',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 849.11,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Samsung LED',
+    image: '/images/LEDtv.jpg',
+    description:
+      'The Ultimate Lighting Solutions for Plant Growth. Plant-centric spectrum for boosting growth and enriching nutrients',
+    brand: 'Samsung',
+    category: 'Electronics',
+    price: 1499.99,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Router',
+    image: '/images/Router.jpg',
+    description:
+      'A router is a networking device that forwards data packets between computer networks.',
+    brand: 'D-Link',
+    category: 'Electronics',
+    price: 221.99,
+    countInStock: 5,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Samsung Galaxy A21',
+    image: '/images/A21.jpg',
+    description:
+      'The phone comes with a 6.50-inch touchscreen display.Samsung Galaxy A21 comes with 3GB of RAM',
+    brand: 'Samsung',
+    category: 'Electronics',
+    price: 170.99,
+    countInStock: 30,
+    rating: 0,
+    numReviews: 0,
+  }
+]
+
+export default products
