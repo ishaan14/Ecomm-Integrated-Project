47099b6b1f999cd2233f2979627f3c680e1143db
Ishaan Garg <ishaangarg.cse19@chitkarauniversity.edu.in>
2022-06-25 17:09:34 +0530

    changes committed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,9 +4,15 @@ import { Container, Row, Col } from 'react-bootstrap'
 const Footer = () => {
   return (
     <footer>
+      <div className='contact'>
+        <h1>Contact Us</h1>
+        <p>Ishaan Garg</p>
+        <p>Kritika</p>
+        <p>Pranshu Goyal</p>
+      </div>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; ShopEasy</Col>
         </Row>
       </Container>
     </footer>
